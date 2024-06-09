20a16757fd957750c903be91bc07a2b9aab7e563
Mamunur Roshid <69715351+mamunurroshid90@users.noreply.github.com>
2024-06-09 12:01:00 +0600

    added product cart

--- components/Products.jsx
+++ components/Products.jsx
@@ -19,7 +19,7 @@ const Products = ({ products }) => {
       </div>
       <div className=" max-w-screen-xl mx-auto py-10 grid grid-cols-4 gap-10">
         {products.map((item) => (
-          <ProductsCart key={item.id} />
+          <ProductsCart key={item.id} product={item} />
         ))}
       </div>
     </div>
--- components/ProductsCart.jsx
+++ components/ProductsCart.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { BsArrowRightShort } from "react-icons/bs";
 
-const ProductsCart = () => {
-  return <div>ProductsCart</div>;
+const ProductsCart = ({ product }) => {
+  // console.log(product);
+  return (
+    <div className=" group">
+      <div className=" w-full h-96 cursor-pointer overflow-hidden">
+        <img
+          className=" w-full h-full object-cover group-hover:scale-110 duration-500"
+          src={product.image}
+          alt="productImage"
+        />
+      </div>
+      <div className=" w-full border-[1px] px-2 py-4">
+        <div className=" flex justify-between items-center">
+          <div>
+            <h2 className=" font-titleFonts text-base font-bold">
+              ${product.title.substring(0, 15)}
+            </h2>
+          </div>
+          <div className=" flex justify-end gap-2 relative overflow-hidden w-28 text-sm">
+            <div className=" flex gap-2 transform group-hover:translate-x-24 transition-transform duration-500">
+              <p className=" line-through text-gray-500">$566</p>
+              <p className=" font-semibold">${product.price}</p>
+            </div>
+            <p className=" absolute z-20 w-[100px] text-gray-500 hover:text-gray-900 flex items-center gap-1 top-0 transform -translate-x-32 group-hover:-translate-x-0 transition-transform cursor-pointer duration-500">
+              add to cart{" "}
+              <span>
+                <BsArrowRightShort />
+              </span>
+            </p>
+          </div>
+        </div>
+        <div>{product.category}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductsCart;
